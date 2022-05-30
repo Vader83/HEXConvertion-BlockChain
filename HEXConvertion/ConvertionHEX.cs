1261a21753186fa346160f4413b99a9f3dfda4b1
Дмитро Олійник <dmytro.oliinyk1@nure.ua>
2022-05-30 23:56:08 +0300

    Add project files.

---
+++ HEXConvertion/ConvertionHEX.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace HEXConvertion
+{
+    public static class ConvertionHEX
+    {
+        public static BigInteger HEXtoInt(string hexString)
+        {
+            return new BigInteger(Convert.ToByte(hexString, 16));
+        }
+
+        public static BigInteger HEXtoInt(int hex)
+        {
+            return new BigInteger();
+        }
+    }
+}
